93cbc9a54e2293468fe556c3674ede9da8e679e0
Olivier Vanduynslaeger <o.vanduynslaeger@gmail.com>
2015-05-15 12:21:05 +0000

    Add favicon

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ server.configure(function(){
     server.use(connect.bodyParser());
     server.use(express.cookieParser());
     server.use(express.session({ secret: "shhhhhhhhh!"}));
+    server.use(express.favicon(__dirname + "/front/static/images/favicon.ico"));     
     server.use(connect.static(__dirname + '/front/static'));
     server.use(server.router);
 });
