5c5ddf4101da6e75316e662e6c1cba2936a3d1cd
Olivier Vanduynslaeger <o.vanduynslaeger@gmail.com>
2015-11-19 12:06:37 +0000

    Add toast to macro screen

--- config/routes.js
+++ config/routes.js
@@ -14,18 +14,8 @@ module.exports = function (app) {
 
 	app.get('/macro', ui_ctrl.macro);
 
-//	app.get('/param', ui_ctrl.param);
-
-//	app.get('/test', ui_ctrl.test);
-
-//	app.get('/clearcache', ui_ctrl.clearcache);
-
     app.get('/api/jeedom/cmd/:id', api_ctrl.runDeviceCommand);
 
     app.get('/api/jeedom/cmd/:id/slide/:slider', api_ctrl.runDeviceCommandSlide);
-
-//    app.get('/api/jeedom/cmd/:id/color/:color', api_ctrl.runDeviceCommandColor);
-
-
 
 };
